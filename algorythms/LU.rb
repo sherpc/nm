28e33d5cb90157639007d3b4e5b2d06d2c1af2c9
sherpc <aleksandrsher@gmail.com>
2012-03-27 18:23:00 +0400

    RF: removed two start loops, increased main loop range

--- algorythms/LU.rb
+++ algorythms/LU.rb
@@ -2,16 +2,8 @@ class LU < Solution
   def decompose
     @result = Array.new(@matrix.length) { Array.new(@matrix[0].length, 0) }
     n = @matrix.length - 1
-    # U, i = 0
-    for j in 0..n
-      @result[0][j] = @matrix[0][j]
-    end
-    # L, j = 0
-    for i in 1..n
-      @result[i][0] = @matrix[i][0] / @result[0][0]
-    end
     # U, L, i = 1..n
-    for i in 1..n
+    for i in 0..n
       @i = i
       for j in i..n
         @result[i][j] = @matrix[i][j] - sum(i,j)

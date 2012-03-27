892270d7f669178e66f791b152e4e7f346cff3a2
sherpc <aleksandrsher@gmail.com>
2012-03-27 18:25:12 +0400

    RF: renaming, more easy lu_m init

--- algorythms/LU.rb
+++ algorythms/LU.rb
@@ -1,20 +1,20 @@
 class LU < Solution
   def decompose
-    @result = Array.new(@matrix.length) { Array.new(@matrix[0].length, 0) }
+    @lu_m = Array.new(@matrix)
     n = @matrix.length - 1
     # U, L, i = 1..n
     for i in 0..n
       @i = i
       for j in i..n
-        @result[i][j] = @matrix[i][j] - sum(i,j)
-        @result[j+1][i] = (@matrix[j+1][i] - sum(j+1,i)) / @result[i][i] if j < n
+        @lu_m[i][j] = @matrix[i][j] - sum(i,j)
+        @lu_m[j+1][i] = (@matrix[j+1][i] - sum(j+1,i)) / @lu_m[i][i] if j < n
       end
     end
-    Solution.new(@result)
+    Solution.new(@lu_m)
   end
 
   def sum(a,b)
-    (0..@i-1).reduce(0) { |s,k| s + @result[a][k] * @result[k][b] }
+    (0..@i-1).reduce(0) { |s,k| s + @lu_m[a][k] * @lu_m[k][b] }
   end
 end
 

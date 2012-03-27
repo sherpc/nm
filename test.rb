5e74db6240e61ed156bc1657ada0f287861cbf87
sherpc <aleksandrsher@gmail.com>
2012-03-27 18:20:32 +0400

    Added LU decomposition test

--- test.rb
+++ test.rb
@@ -24,7 +24,8 @@ class TestLU < Test::Unit::TestCase
   def test_lu_decompose
     test_cases = [
       {data: [[10,1,1], [2,10,1], [2,2,10]], expected: [[10,1,1], [0.2,9.8,0.8], [0.2,0.18,9.65]]},
-      {data: [[4,3], [6,3]], expected: [[4,3], [1.5, -1.5]]}
+      {data: [[4,3], [6,3]], expected: [[4,3], [1.5, -1.5]]},
+      {data: [[13, 7, 4], [7, 9, -3], [4, -3, 9]], expected: [[13, 7, 4], [0.54, 5.23, -5.15], [0.31, -0.99, 2.69]]}
     ]
     test_cases.each { |test_case| do_lu_test(test_case[:data], test_case[:expected]) }
   end

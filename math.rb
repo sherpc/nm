fe4155cf0e67be821a4ddd25c6a379b995fc73fd
sherpc <aleksandrsher@gmail.com>
2012-03-28 17:00:38 +0400

    Released Math.sum method

--- math.rb
+++ math.rb
@@ -1,5 +1,5 @@
 class << Math
-  def S(a,b)
-    a + b
+  def sum(from,to,&block)
+    (from..to).reduce(0) { |sum, i| sum + yield(i) }
   end
 end
--- tests/math.rb
+++ tests/math.rb
@@ -2,7 +2,18 @@ require "test/unit"
 require "./math.rb"
 
 class TestMath < Test::Unit::TestCase
+  def setup
+    @array = [1, 2, 3]
+    @array2 = [4, 5, 6]
+  end
+
   def test_sum
-    assert_equal Math.S(1,2), 3
+    sum = Math.sum(0, 2) { |i| @array[i] }
+    assert_equal sum, 6
+  end
+
+  def test_more_complex_sum
+    sum = Math.sum(0, 2) { |i| @array[i] * @array2[i] }
+    assert_equal sum, 32
   end
 end

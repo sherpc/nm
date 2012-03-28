e27fd5c4b4c17ecae4f94dbe2c15609a72ae0797
sherpc <aleksandrsher@gmail.com>
2012-03-28 17:35:46 +0400

    RF: .reduce replaced by Math.sum

--- algorythms/LU.rb
+++ algorythms/LU.rb
@@ -1,11 +1,8 @@
 class LU < Solution
   attr_reader :lu_m, :answer
   alias lu_matrix lu_m
-
-  def new_matrix
-    Array.new(@n+1) { Array.new(@n+1) }
-  end
-
+  
+  # LU decomposition
   def decompose
     return unless @lu_m.nil?
     @lu_m = new_matrix
@@ -20,32 +17,10 @@ class LU < Solution
   end
   
   def sum_lu(a,b)
-    (0..@i-1).reduce(0) { |s,k| s + @lu_m[a][k] * @lu_m[k][b] }
+    Math.sum(0,@i-1) { |k| @lu_m[a][k] * @lu_m[k][b] }
   end
 
-  def unit_vector(e)
-    Array.new(@n+1) { |i| i == e ? 1 : 0 }
-  end
-
-  def invert
-    @inv_m = new_matrix
-    for i in 0..@n
-      solve(unit_vector i)
-      (0..@n).each { |j| @inv_m[j][i] = @answer[j] }
-    end
-  end
-
-  def det
-    decompose if @lu_m.nil?
-    @det unless @det.nil?
-    @det = (0..@n).reduce(1) { |mul,i| mul * @lu_m[i][i] }
-  end
-
-  def invertible_matrix
-    invert if @inv_m.nil?
-    @inv_m
-  end
-
+  # Solving SoLE
   # b -- free values colum
   def solve(vector)
     @b = vector
@@ -56,16 +31,46 @@ class LU < Solution
 
   def eval_z
     @z = Array.new(@lu_m.length)
-    for i in 0..@n
-      @z[i] = @b[i] - (0..i-1).reduce(0) { |s,j| s + @lu_m[i][j] * @z[j] }
-    end
+    (0..@n).each { |i| z i }
+  end
+
+  def z i
+    @z[i] = @b[i] - Math.sum(0,i-1) { |j| @lu_m[i][j] * @z[j] }
   end
 
   def eval_answer
     @answer = Array.new(@z.length)
-    for i in @n.downto(0)
-      @answer[i] = (@z[i] - (i+1..@n).reduce(0) { |s,j| s + @lu_m[i][j]*@answer[j] }) / @lu_m[i][i]
+    @n.downto(0) { |i| x i }
+  end
+
+  def x i
+    @answer[i] = (@z[i] - Math.sum(i+1,@n) { |j| @lu_m[i][j] * @answer[j] }) / @lu_m[i][i]
+  end
+
+  # Invertible matrix
+  def invertible_matrix
+    invert if @inv_m.nil?
+    @inv_m
+  end
+
+  def invert
+    @inv_m = new_matrix
+    for i in 0..@n
+      solve(unit_vector i)
+      (0..@n).each { |j| @inv_m[j][i] = @answer[j] }
     end
   end
+
+  def unit_vector(e)
+    Array.new(@n+1) { |i| i == e ? 1 : 0 }
+  end
+  
+  # Determinant
+  def det
+    decompose if @lu_m.nil?
+    @det unless @det.nil?
+    @det = (0..@n).reduce(1) { |mul,i| mul * @lu_m[i][i] }
+  end
+
 end
 

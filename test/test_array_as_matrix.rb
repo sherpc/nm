a1969a5869909e2ca9cf93c5ec4f2b9d882af9b0
Aleksandr Sher <aleksandrsher@gmail.com>
2012-04-27 14:14:52 +0400

    Added multiplication tests

---
+++ test/test_array_as_matrix.rb
@@ -0,0 +1,14 @@
+require "test/unit"
+require "./main.rb"
+
+class TestArrayAsMatrix < Test::Unit::TestCase
+  def setup
+    @m3 = [[1,-2,3],[-2,-7,10],[0,4,2]]
+  end
+
+  def test_multiple_matrix_vector()
+    assert_equal @m3 ** [0,0,0], [0,0,0]
+    assert_equal @m3 ** [1,1,1], [2,1,6]
+    assert_equal @m3 ** [2,1,3], [9,19,10]
+  end
+end

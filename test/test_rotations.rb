76ed32bbde04c81739b6847d49a8c030a5b1768a
Aleksandr Sher <aleksandrsher@gmail.com>
2012-04-29 20:43:25 +0400

    Added Rotations class

--- test/test_rotations.rb
+++ test/test_rotations.rb
@@ -4,5 +4,6 @@ require "./main.rb"
 class TestRotations < Test::Unit::TestCase
   def test_phi
     assert_equal Rotations.phi(1,2,2), Math::PI/4
+    assert_not_equal Rotations.phi(1,2,3), Math::PI/4
   end
 end

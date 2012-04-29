8946862264159d764e154f8a91749e3ddea44ed3
Aleksandr Sher <aleksandrsher@gmail.com>
2012-04-29 20:38:16 +0400

    Added Rotations class

---
+++ algorythms/rotations.rb
@@ -0,0 +1,6 @@
+class Rotations < Solution
+  def self.phi a_i_j, a_i_i, a_j_j
+    return Math::PI / 4 if a_i_i == a_j_j
+    0.5 * Math.atan((2 * a_i_j) / (a_i_i - a_j_j))
+  end
+end
---
+++ test/test_rotations.rb
@@ -0,0 +1,8 @@
+require "test/unit"
+require "./main.rb"
+
+class TestRotations < Test::Unit::TestCase
+  def test_phi
+    assert_equal Rotations.phi(1,2,2), Math::PI/4
+  end
+end

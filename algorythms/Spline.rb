9d06a16205641124b5331699003c0c83746a166b
sherpc <aleksandrsher@gmail.com>
2012-06-04 02:29:37 +0400

    Spline pdf example works

--- algorythms/Spline.rb
+++ algorythms/Spline.rb
@@ -6,12 +6,51 @@ class Spline
     (1..@n).each { |i| @h[i] = x[i] - x[i-1] }
   end
 
-  def coefficients
-    b = (2..@n).map { |i| 3 * (coeff_b(i) - coeff_b(i-1)) }
-    p b
+  def create
+    @a = [0, *(1..@n).map { |i| @f[i-1] }]
+    @c = [0,0,*calculate_c]
+    @b = [0,*(1..@n).map { |i| calculate_b(i) }]
+    @d = [0,*(1..@n).map { |i| calculate_d(i) }]
   end
 
-  def coeff_b i
+  def[] x
+    create unless @a
+    i = x.ceil
+    throw "x: #{x} in not valid range" unless 1 <= i && i <= @n
+    @a[i] + @b[i] * term(x,i,1) + @c[i] * term(x,i,2) + @d[i] * term(x,i,3)
+  end
+
+  def term x, i, power
+    (x - @x[i-1]) ** power
+  end
+
+  def calculate_b i
+    return (@f[i] - @f[i-1]) / @h[i] - (2.0/3) * @h[i] * @c[i] if i == @n
+    (@f[i] - @f[i-1]) / @h[i] - (1.0/3) * @h[i] * (@c[i+1] + 2 * @c[i])
+  end
+
+  def calculate_d i
+    return  -@c[i] / (3.0 * @h[i]) if i == @n
+    (@c[i+1] - @c[i]) / (3.0 * @h[i])
+  end
+
+  def calculate_c
+    a = (3..@n).map { |i| coeff_a(i) }
+    b = (2..@n).map { |i| coeff_b(i) }
+    c = (2..@n-1).map { |i| coeff_c(i) }
+    d = (2..@n).map { |i| 3 * (coeff_d(i) - coeff_d(i-1)) }
+    m = Thomas.new [a,b,c,d]
+    m.solve
+    m.answer
+  end
+
+  def coeff_d i
       (@f[i] - @f[i-1]) / @h[i]
   end
+
+  def coeff_a i; @h[i-1]; end
+  def coeff_b i
+    2 * (@h[i-1] + @h[i])
+  end
+  def coeff_c i; @h[i]; end
 end
--- test/test_spline.rb
+++ test/test_spline.rb
@@ -5,10 +5,13 @@ class TestSpline < Test::Unit::TestCase
   def setup
     @x = [0,1,2,3,4].map { |x| x.to_f }
     @f = [0,1.8415,2.9093,3.1411,3.2432]
+
+    @x_l = []
   end
 
   def test_tmp
     sp = Spline.new @x, @f
-    sp.coefficients
+    sp.create
+    assert_equal 2.4969642857142857, sp[1.5]
   end
 end
